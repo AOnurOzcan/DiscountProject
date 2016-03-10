051c70924d3a14c09c5783a2037ae28d249681a1
AOnurOzcan <aonurozcan@gmail.com>
2016-03-10 22:07:13 +0200

    -Projenin portu 80 olarak değiştirildi _Ürün ekleme sayfası kısmen tamamlandı. -jasny bootstrap kütüphanesi eklendi

--- Web/src/public/app/Product/productView.js
+++ Web/src/public/app/Product/productView.js
@@ -26,7 +26,6 @@ define([
     url: "/allProducts"
   });
 
-
   var AddProductView = core.CommonView.extend({
     autoLoad: true,
     el: ".page",
@@ -36,28 +35,49 @@ define([
     },
     initialize: function () {
       this.mainCategoryCollection = new MainCategoryCollection();
-      this.subCategorySelectView = new SubCategorySelectView();
     },
     getSubCategories: function () {
-      var that = this;
       var subCategoryCollection = new SubCategoryCollection([], {parentCategoryId: $("#productMainCategorySelect").val()});
       subCategoryCollection.fetch({
         success: function (subCategories) {
-          that.subCategorySelectView.render(subCategories.toJSON());
+          $("#productSubCategorySelect").html(subCategorySelectTemplate({subCategories: subCategories.toJSON()}));
         }
       });
 
     },
     addProduct: function (e) {
       e.preventDefault();
-      var form = this.form();
-      var values = form.getValues;
-      console.log($("input:file").val());
+
+      var form = $("#addProductForm")[0];
+      var oData = new FormData(form);
+      var oReq = new XMLHttpRequest();
+      oReq.open("POST", "/testUpload", true);
+      $("#addProductButton").attr("disabled", true);
+      oReq.send(oData);
+
+      oReq.onload = function (oEvent) {
+        if (oReq.status == 200) {
+          alert("Dosya yükleme başarılır");
+          $("#addProductButton").attr("disabled", false);
+        } else {
+          alert("Dosya yükleme başarısız!!!");
+          $("#addProductButton").attr("disabled", false);
+        }
+      };
+
+
     },
     render: function () {
       this.$el.html(addProductTemplate({
         mainCategories: this.mainCategoryCollection.toJSON()
       }));
+      this.$el.on('change.bs.fileinput', function (abc) {
+        var fileName = $("#imageURL").val().replace(/^.*\./, '');
+        if (!(fileName == "png" || fileName == "jpg")) {
+          $(".fileinput").fileinput('clear');
+          alert("Yanlış Dosya Uzantısı");
+        }
+      });
     }
   });
 
@@ -69,12 +89,6 @@ define([
     },
     render: function () {
       this.$el.html(listProductTemplate({products: this.productCollection.toJSON()}));
-    }
-  });
-
-  var SubCategorySelectView = Backbone.View.extend({
-    render: function (subCategories) {
-      $("#productSubCategorySelect").html(subCategorySelectTemplate({subCategories: subCategories}));
     }
   });
 
--- Web/src/public/app/Util/outer.js
+++ Web/src/public/app/Util/outer.js
@@ -4,17 +4,11 @@ window.define(['backbone'], function (Backbone) {
 
     showView: function (view, params) {
 
-      // Bu fonksiyona parametre oliarak verilen view'ler birden fazla ise
-      var $content = $(".page");
-      $content.children().detach();
-
       view.forEach(function (view) {
         view.params = params;
         view.load.call(view, params);
       });
 
-      $content.hide();
-      $content.fadeIn(500);
     }
   });
 });
