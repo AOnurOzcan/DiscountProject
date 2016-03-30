b3051809a63440cf1d3ecd99414c51920b283592
ramos41 <ramazanakkas95@gmail.com>
2016-03-30 05:53:33 +0300

    Uygulama açıkken bildirime tıklama eklendi. Menuler duzenlendı

--- Mobile/MyApplication/app/src/main/java/com/example/ooar/discountproject/fragment/UserTabsFragment.java
+++ Mobile/MyApplication/app/src/main/java/com/example/ooar/discountproject/fragment/UserTabsFragment.java
@@ -1,15 +1,20 @@
 package com.example.ooar.discountproject.fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TabHost;
 
 import com.example.ooar.discountproject.R;
+import com.example.ooar.discountproject.activity.UserActivity;
 
 /**
  * Created by Onur Kuru on 25.3.2016.
@@ -23,6 +28,7 @@ public class UserTabsFragment extends Fragment {
         if (mTabHost == null) {
             return inflater.inflate(R.layout.user_tabs_layout, container, false);
         } else return mTabHost;
+
     }
 
     @Override
@@ -33,6 +39,22 @@ public class UserTabsFragment extends Fragment {
 
             mTabHost.addTab(mTabHost.newTabSpec("tab1").setIndicator("Bildirimler", null), NotificationsFragment.class, null);
             mTabHost.addTab(mTabHost.newTabSpec("tab2").setIndicator("Tercihler", null), ChoisesFragment.class, null);
+            UserTabsFragment.mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+                public void onTabChanged(String tabId) {
+                    switch (UserTabsFragment.mTabHost.getCurrentTab()) {
+                        case 0:
+                            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Bildirimler");
+                            UserActivity.mTitle = "Bildirimler";
+                            break;
+                        case 1:
+                            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Tercihler");
+                            UserActivity.mTitle = "Tercihler";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            });
         }
     }
 }
--- Mobile/MyApplication/app/src/main/java/com/example/ooar/discountproject/gcm/GcmBroadcastReceiver.java
+++ Mobile/MyApplication/app/src/main/java/com/example/ooar/discountproject/gcm/GcmBroadcastReceiver.java
@@ -15,16 +15,20 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.opengl.Visibility;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.ooar.discountproject.R;
 import com.example.ooar.discountproject.activity.MainActivity;
 import com.example.ooar.discountproject.activity.UserActivity;
 import com.example.ooar.discountproject.fragment.NotificationDetailFragment;
+import com.example.ooar.discountproject.fragment.UserTabsFragment;
+import com.example.ooar.discountproject.util.FragmentChangeListener;
 
 public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
     private static final int MY_NOTIFICATION_ID = 1;
@@ -32,29 +36,36 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
     NotificationManager notificationManager;
     Notification myNotification;
     int notificationId = 0;
+    String notificationTitle;
+    String notificationContent;
+    Intent myIntent;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        String tempReceive = intent.getExtras().getString("key1");
-        if (tempReceive != null) {
-            notificationId = Integer.parseInt(tempReceive);
-            number = context.getSharedPreferences("Session", Activity.MODE_PRIVATE).getInt("NotificationCount", 0);
-            if (number == 0) {
-                Intent myIntent = new Intent(context, MainActivity.class);
-                myIntent.putExtra("notificationId", notificationId);
+        String Temp = intent.getExtras().getString("notificationId");
+        if (Temp != null) {
 
-                PendingIntent pendingIntent = PendingIntent.getActivity(
-                        context,
-                        0,
-                        myIntent,
-                        Intent.FLAG_ACTIVITY_NEW_TASK);
+            notificationId = Integer.parseInt(intent.getExtras().getString("notificationId"));
+            notificationTitle = intent.getExtras().getString("notificationTitle");
+            notificationContent = intent.getExtras().getString("notificationContent");
+            number = context.getSharedPreferences("Session", Activity.MODE_PRIVATE).getInt("NotificationCount", 0);
+
+            if (number == 0) {
+                if (UserActivity.isOpen) {
+                    myIntent = new Intent(context, UserActivity.class);
+                    myIntent.putExtra("notificationId", notificationId);
+                    myIntent.putExtra("fragmentName", "notificationDetailFragment");
+                }
+
                 number++;
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, myIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
                 SharedPreferences.Editor editor = context.getSharedPreferences("Session", context.MODE_PRIVATE).edit();
                 editor.putInt("NotificationCount", number).commit();
+                editor.putInt("notificationId", notificationId).commit();
                 myNotification = new NotificationCompat.Builder(context)
-                        .setContentTitle("One!")
-                        .setContentText("Do Something...")
-                        .setTicker("Notification!")
+                        .setContentTitle(notificationTitle)
+                        .setContentText(notificationContent)
+                        .setTicker("1 Yeni İndirim!")
                         .setWhen(System.currentTimeMillis())
                         .setContentIntent(pendingIntent)
                         .setDefaults(Notification.DEFAULT_SOUND)
@@ -63,19 +74,18 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
                         .build();
                 // editor.putInt("NotificationId", Integer.parseInt(intent.getExtras().getString("key1"))).commit();
             } else {
-                Intent myIntent = new Intent(context, MainActivity.class);
-                PendingIntent pendingIntent = PendingIntent.getActivity(
-                        context,
-                        0,
-                        myIntent,
-                        Intent.FLAG_ACTIVITY_NEW_TASK);
+                if (UserActivity.isOpen) {
+                    myIntent = new Intent(context, UserActivity.class);
+                    myIntent.putExtra("fragmentName", "notificationsFragment");
+                }
                 number++;
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, myIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
                 SharedPreferences.Editor editor = context.getSharedPreferences("Session", context.MODE_PRIVATE).edit();
                 editor.putInt("NotificationCount", number).commit();
                 myNotification = new NotificationCompat.Builder(context)
-                        .setContentTitle(number + " yeni Bildirim!")
-                        .setContentText("Do Something...")
-                        .setTicker("Notification!")
+                        .setContentTitle("indirimİN")
+                        .setContentText("Yeni İnrimlerin var!")
+                        .setTicker(number + " yeni İndirim!")
                         .setWhen(System.currentTimeMillis())
                         .setContentIntent(pendingIntent)
                         .setDefaults(Notification.DEFAULT_SOUND)
@@ -85,10 +95,8 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
                 editor.putInt("NotificationId", 0).commit();
             }
 
-            notificationManager =
-                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
             notificationManager.notify(MY_NOTIFICATION_ID, myNotification);
         }
-
     }
 }
