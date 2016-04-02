f7e20b1e6fd8aa3edf3ee92634f91f112f177fb1
Onur Kuru <oonurkuru@gmail.com>
2016-04-02 20:17:40 +0300

    resim cachelemede max cache size düzeltidi

--- Mobile/MyApplication/app/src/main/java/com/example/ooar/discountproject/util/ImageCache.java
+++ Mobile/MyApplication/app/src/main/java/com/example/ooar/discountproject/util/ImageCache.java
@@ -2,22 +2,8 @@ package com.example.ooar.discountproject.util;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.os.Environment;
 import android.support.v4.util.LruCache;
-import android.util.Log;
 
-import com.example.ooar.discountproject.BuildConfig;
-import com.jakewharton.disklrucache.DiskLruCache;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * Created by Onur Kuru on 2.4.2016.
@@ -26,7 +12,7 @@ public class ImageCache {
     private LruCache<String, Bitmap> mMemoryCache;
     private Context context;
     private final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-    private final int cacheSize = maxMemory / 800;
+    private final int cacheSize = maxMemory / 8;
 
     public ImageCache(Context context) {
         this.context = context;
